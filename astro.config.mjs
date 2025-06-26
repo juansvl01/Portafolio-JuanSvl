5c09e1605cf87c44552a1f8411b44710826c1327
Juan Sebastian Vargas <juan.svl01@gmail.com>
2025-06-26 02:07:49 -0500

    ya no se que hacer

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   output: 'static',
-  adapter: github(),
+  adapter: sataticAdapter(),
   site: 'https://juansv101.github.io',
   base: 'Portafolio-JuanSvl',
   vite: {
