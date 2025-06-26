a617ed1a428bc4a087a1686f741e750dae7f4e15
Juan Sebastian Vargas <juan.svl01@gmail.com>
2025-06-26 00:54:10 -0500

    Primera versión del portafolio

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  output: 'static',
+  adapter: github(),
   site: 'https://juansv101.github.io/Portafolio-JuanSvl/',
   base: '/Portafolio-JuanSvl/',
   vite: {
