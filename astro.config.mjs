999637ec608cedc9aec0f9af81c8e6179eacf6ff
Juan Sebastian Vargas <juan.svl01@gmail.com>
2025-06-26 01:26:30 -0500

    workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
+import github from '@astrojs/github-pages';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   output: 'static',
   adapter: github(),
-  site: 'https://juansv101.github.io/Portafolio-JuanSvl/',
-  base: '/Portafolio-JuanSvl/',
+  site: 'https://juansv101.github.io',
+  base: 'Portafolio-JuanSvl',
   vite: {
     plugins: [tailwindcss()]
   }
